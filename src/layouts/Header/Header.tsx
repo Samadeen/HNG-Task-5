efb8fd0e104adc3f0f9f4123e8a67ef7e537fb39
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-10-04 10:46:14 +0100

    Update Header.tsx

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,14 +1,18 @@
 import styles from './Header.module.scss';
+import logo from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <img src='' alt='' />
+      <Link to='/'>
+        <img src={logo} alt='logo' />
+      </Link>
       <ul>
         <li>Features</li>
         <li>How it works</li>
       </ul>
-      <button>Get STarted</button>
+      <Link to='/auth'>Get Started</Link>
     </header>
   );
 };
